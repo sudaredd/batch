d7c98909dfa21d3f718ecfc44a4856bd49f32d7e
sudarsana kasireddy <sudaredd@gmail.com>
2021-11-30 18:40:06 -0500

    Handle rejections when queue is full

--- src/main/java/app/batch/batch/async/AsyncAppRunner.java
+++ src/main/java/app/batch/batch/async/AsyncAppRunner.java
@@ -28,9 +28,13 @@ public class AsyncAppRunner implements CommandLineRunner {
         CompletableFuture<User> page2 = gitHubLookupService.findUser("CloudFoundry");
         CompletableFuture<User> page3 = gitHubLookupService.findUser("Spring-Projects");
         CompletableFuture<User> page4 = gitHubLookupService.findUser("sudaredd");
+        CompletableFuture<User> page5 = gitHubLookupService.findUser("real-logic");
+        CompletableFuture<User> page6 = gitHubLookupService.findUser("dsyer");
+        CompletableFuture<User> page7 = gitHubLookupService.findUser("rstoyanchev");
+        CompletableFuture<User> page8 = gitHubLookupService.findUser("joshlong");
 
         // Wait until they are all done
-        CompletableFuture.allOf(page1, page2, page3, page4).join();
+        CompletableFuture.allOf(page1, page2, page3, page4, page5, page6, page7, page8).join();
 
         // Print results, including elapsed time
         log.info("Elapsed time: " + (System.currentTimeMillis() - start));
@@ -38,6 +42,10 @@ public class AsyncAppRunner implements CommandLineRunner {
         log.info("--> " + page2.get());
         log.info("--> " + page3.get());
         log.info("--> " + page4.get());
+        log.info("--> " + page5.get());
+        log.info("--> " + page6.get());
+        log.info("--> " + page7.get());
+        log.info("--> " + page8.get());
 
 
     }
--- src/main/java/app/batch/batch/config/ConcurrentExecutorConfig.java
+++ src/main/java/app/batch/batch/config/ConcurrentExecutorConfig.java
@@ -1,10 +1,14 @@
 package app.batch.batch.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
+import java.util.concurrent.ThreadPoolExecutor;
 
 @Configuration
 public class ConcurrentExecutorConfig {
@@ -12,12 +16,30 @@ public class ConcurrentExecutorConfig {
     @Bean(name = "taskConcurrentExecutor")
     public Executor taskConcurrentExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        executor.setCorePoolSize(3);
-        executor.setMaxPoolSize(3);
-        executor.setQueueCapacity(500);
+        executor.setCorePoolSize(1);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(1);
         executor.setThreadNamePrefix("GithubLookup-");
         executor.initialize();
+        executor.setRejectedExecutionHandler(new BlockCallerExecutionPolicy());
+
         return executor;
+    }
+
+    @Slf4j
+    static class BlockCallerExecutionPolicy implements RejectedExecutionHandler {
+
+        @Override
+        public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+            try {
+                log.info("placing a task to blocking queue in BlockCallerExecutionPolicy");
+                // based on the BlockingQueue documentation below should block until able to place on the queue...
+                executor.getQueue().put(r);
+            }
+            catch (InterruptedException e) {
+                throw new RejectedExecutionException("Unexpected InterruptedException while waiting to add Runnable to ThreadPoolExecutor queue...", e);
+            }
+        }
     }
 
 }
